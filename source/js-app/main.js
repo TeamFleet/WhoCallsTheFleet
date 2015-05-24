55d656172c1ad2401aa0d9efd02595b8783746a5
Diablohu <diablohudream@gmail.com>
2015-05-25 00:53:41 +0800

    [CORE] 加入NPM组件：request-progress

--- source/js-app/main.js
+++ source/js-app/main.js
@@ -3,7 +3,9 @@
 	node.require('nedb')
 	node.require('mkdirp')
 	node.require('request')
+	node.require('request-progress')
 	node.require('semver')
+	node.require('url')
 	var Q = node.require('q')
 		,markdown = node.require( "markdown" ).markdown
 
