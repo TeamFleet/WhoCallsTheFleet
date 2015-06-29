71b8dafac2f4a1a78d635915296652d3136c42c7
Diablohu <diablohudream@gmail.com>
2015-06-30 04:48:07 +0800

    db load: arsenal_weekday

--- source/js-app/main.js
+++ source/js-app/main.js
@@ -708,6 +708,16 @@ _frame.app_main = {
 										_g.data[db_name] = {}
 									_done(db_name)
 									break;
+								case 'arsenal_weekday':
+									_g.data['arsenal_weekday'] = {}
+									_db.arsenal_weekday.find({}).sort({
+										'weekday': 1
+									}).exec(function(err, docs){
+										for(var i in docs){
+											_g.data['arsenal_weekday'][parseInt(i)]
+												= docs[i].improvements
+										}
+									})
 								default:
 									_db[db_name].find({}, function(dberr, docs){
 										if( dberr ){
