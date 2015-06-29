9c2431975356e38a46552870efcc81df77b924c5
Diablohu <diablohudream@gmail.com>
2015-06-30 04:33:37 +0800

    _g.error now always throw error

--- source/js-app/errorlog.js
+++ source/js-app/errorlog.js
@@ -1,19 +1,21 @@
-// 
+//
 _g.error = function( err ){
+	if( !(err instanceof Error) )
+		err = new Error(err)
+
 	_g.log(err)
 
 	node.fs.appendFileSync(
 		node.path.join(_g.execPath, 'errorlog.txt'),
 		new Date()
 		+ "\r\n"
-		+ ( (err instanceof Error)
-			? err.message || ''
-			: err
-		)
+		+ err.message
 		+ "\r\n"
 		+ "\r\n"
 		+ "========================================"
 		+ "\r\n"
 		+ "\r\n"
 	)
+
+	throw err
 }
--- source/js-app/main.js
+++ source/js-app/main.js
@@ -112,11 +112,6 @@
 		if( debugmode )
 			console.log(log)
 	}
-	_g.error = function(err){
-		if( typeof err != 'object' )
-			err = new Error(err)
-		throw err
-	}
 
 
 
