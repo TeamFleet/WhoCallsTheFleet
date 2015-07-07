09789fff6ad963919306b8109466bec0a196d5b0
Diablohu <diablohudream@gmail.com>
2015-07-07 08:00:43 +0800

    try some ES6

--- source/js-app/items/!.js
+++ source/js-app/items/!.js
@@ -1,8 +1,11 @@
-var ITEM = function(){}
-
-ITEM.prototype.getName = function(language){
-	language = language || _g.lang
-	return this['name']
-			? (this['name'][language] || this['name'])
-			: null
+class ITEM {
+	constructor() {
+	}
+	
+	getName(language){
+		language = language || _g.lang
+		return this['name']
+				? (this['name'][language] || this['name'])
+				: null
+	}
 }
--- source/js-app/items/equipment.js
+++ source/js-app/items/equipment.js
@@ -1,24 +1,25 @@
-var Equipment = function( data ){
-	$.extend(true, this, data)
-}
-Equipment.prototype = Object.create(ITEM.prototype)
+class Equipment extends ITEM{
+	constructor(data) {
+		$.extend(true, this, data)
+	}
+	
+	getName(small_brackets, language){
+		language = language || _g.lang
+		var result = ITEM.prototype.getName.call(this, language)
+			,small_brackets_tag = small_brackets && !small_brackets === true ? small_brackets : 'small'
+		return small_brackets
+				? result.replace(/（([^（^）]+)）/g, '<'+small_brackets_tag+'>($1)</'+small_brackets_tag+'>')
+				: result
+	}
+	
+	getType(language){
+		language = language || _g.lang
+		return this['type']
+				? _g['data']['item_types'][this['type']]['name'][language]
+				: null
+	}
 
-Equipment.prototype.getName = function(small_brackets, language){
-	language = language || _g.lang
-	var result = ITEM.prototype.getName.call(this, language)
-		,small_brackets_tag = small_brackets && !small_brackets === true ? small_brackets : 'small'
-	return small_brackets
-			? result.replace(/（([^（^）]+)）/g, '<'+small_brackets_tag+'>($1)</'+small_brackets_tag+'>')
-			: result
-}
-
-Equipment.prototype.getType = function(language){
-	language = language || _g.lang
-	return this['type']
-			? _g['data']['item_types'][this['type']]['name'][language]
-			: null
-}
-
-Equipment.prototype.getIconId = function(){
-	return _g.data.item_types[this['type']]['icon']
+	getIconId(){
+		return _g.data.item_types[this['type']]['icon']
+	}
 }
