65286b784d804389b2b85fae72ae8fa5637944d1
Diablohu <diablohudream@gmail.com>
2015-07-28 18:09:54 +0800

    Ship.getSpeed() & Ship.getRange()

--- source/js-app/items/ship.js
+++ source/js-app/items/ship.js
@@ -1,3 +1,60 @@
+/* Class: Ship / 舰娘
+
+ *******************************************************************
+
+new Ship( Object data )
+	data	原始数据
+
+ *******************************************************************
+
+ship instanceof Ship
+
+ship.getName( joint, language )
+	获取舰名
+	joint		[OPTIONAL]
+		String		连接符，如果存在后缀名，则在舰名和后缀名之间插入该字符串
+		Boolean		如果为 true，则添加默认连接符
+					如果为 false，则不添加连接符
+		null		不添加连接符
+	language	[OPTIONAL]
+		String		语言代码，默认为 _g.lang
+	返回值
+		String		舰名 + 连接符（如果有） + 后缀名（如果有）
+	快捷方式
+		ship._name	默认连接符，默认语言
+
+ship.getSuffix( language )
+	获取后缀名
+	language	[OPTIONAL]
+		String		语言代码，默认为 _g.lang
+	返回值
+		String		后缀名
+
+ship.getType( language )
+	获取舰种
+	language	[OPTIONAL]
+		String		语言代码，默认为 _g.lang
+	返回值
+		String		舰种
+	快捷方式
+		ship._type	默认语言
+
+ship.getSeriesData()
+	获取系列数据
+	返回值
+		Object		系列
+
+ship.getPic( picId )
+	获取图鉴uri/path
+	picId	[OPTIONAL]
+		Number		图鉴Id，默认 0
+	返回值
+		String		uri/path
+	快捷方式
+		ship._pics	获取全部图鉴，Array
+
+ */
+
 class Ship extends ITEM{
 	constructor(data){
 		super()
@@ -7,15 +64,12 @@ class Ship extends ITEM{
 	getName(joint, language){
 		joint = joint || ''
 		language = language || _g.lang
+		let suffix = this.getSuffix(language)
 		return (
 				this['name'][language] || this['name']['ja_jp']
-				) + (
-				this['name'].suffix ? (
+				) + ( suffix ? (
 						(joint === true ? _g.joint : joint)
-						+ (
-								_g.data['ship_namesuffix'][this['name'].suffix][language]
-								|| _g.data['ship_namesuffix'][this['name'].suffix]['ja_jp']
-							)
+						+ suffix
 					) : ''
 				)
 	}
@@ -37,6 +91,9 @@ class Ship extends ITEM{
 				? _g['data']['ship_types'][this['type']]['full_zh']
 				: null
 	}
+	get _type(){
+		return this.getType()
+	}
 	
 	getSeriesData(){
 		return this['series']
@@ -46,7 +103,7 @@ class Ship extends ITEM{
 	
 	getPic(picId){
 		let series = this.getSeriesData()
-		picId = parseInt(picId)
+		picId = parseInt(picId || 0)
 		
 		for(let i in series){
 			if( series[i].id == this.id ){
@@ -78,5 +135,21 @@ class Ship extends ITEM{
 			arr.push( this.getPic(i) )
 		}
 		return arr
+	}
+	
+	getSpeed(language){
+		language = language || _g.lang
+		return _g.statSpeed[parseInt(this.stat.speed)]
+	}
+	get _speed(){
+		return this.getSpeed()
+	}
+	
+	getRange(language){
+		language = language || _g.lang
+		return _g.statRange[parseInt(this.stat.range)]
+	}
+	get _range(){
+		return this.getRange()
 	}
 }
