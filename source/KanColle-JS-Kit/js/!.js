efc9c639a68aa53d524f70017618775bf9826d9b
Diablohu <diablohudream@gmail.com>
2015-09-29 06:26:13 +0800

    remove KanColle-JS-Kit folder

--- source/KanColle-JS-Kit/js/!.js
+++
@@ -1,8 +0,0 @@
-// put this file before all js files when compile with a builder
-
-"use strict";
-
-if( typeof _g == 'undefined' )
-	var _g = {}
-
-_g.lang = _g.lang || 'zh_cn' 
--- source/KanColle-JS-Kit/js/class-item/entity.js
+++
@@ -1,8 +0,0 @@
-// Class for Entity (Person/Group, such as CVs, illustrators)
-
-class Entity extends ItemBase{
-	constructor(data) {
-		super()
-		$.extend(true, this, data)
-	}
-}
