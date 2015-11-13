dffb617c9072dcf92506447b8ca7c77cef68ecb3
Diablohu <diablohudream@gmail.com>
2015-11-14 05:59:16 +0800

    update modal

--- dev-output/assets-output/js.js
+++ dev-output/assets-output/js.js
@@ -1931,83 +1931,87 @@ _frame.modal = {
 	},
 
 	show: function show(content, title, options, callback) {
-		clearTimeout(_frame.modal.hide_timeout);
-		_frame.modal.hide_timeout = null;
+		clearTimeout(this.hide_timeout);
+		this.hide_timeout = null;
 
-		_frame.modal.dom.container.addClass('show');
-		_frame.modal.showing = true;
+		this.dom.container.addClass('show');
+		this.showing = true;
 
-		var settings = $.extend({}, _frame.modal.defaults, options);
+		var settings = $.extend({}, this.defaults, options);
 
-		content.appendTo(_frame.modal.dom.content);
+		content.appendTo(this.dom.content);
 
 		if (title) {
-			_frame.modal.dom.titlebar.html(title);
-			_frame.modal.dom.container.addClass('hastitle');
+			this.dom.titlebar.html(title);
+			this.dom.container.addClass('hastitle');
 		} else {
-			_frame.modal.dom.titlebar.html('');
-			_frame.modal.dom.container.removeClass('hastitle');
+			this.dom.titlebar.html('');
+			this.dom.container.removeClass('hastitle');
 		}
 
-		_frame.modal.dom.box.css({
+		this.dom.box.css({
 			'width': settings.width || null,
 			'height': settings.height || null
 		});
 
 		if (settings.showBlured) {
-			if (!_frame.modal.dom.blured && typeof node != 'undefined') node.win.capturePage(function (datauri) {
-				_frame.modal.dom.blured = $('<img/>').attr('src', datauri).appendTo(_frame.modal.dom.bg);
-			}, 'jpg', 'datauri');
-			_frame.modal.dom.container.addClass('bluredbg');
+			if (_huCss.csscheck_full('backdrop-filter')) {
+				this.dom.container.addClass('mod-blur-backdrop');
+			} else if (!this.dom.blured && typeof node != 'undefined') {
+				node.win.capturePage(function (datauri) {
+					_frame.modal.dom.blured = $('<img/>').attr('src', datauri).appendTo(_frame.modal.dom.bg);
+				}, 'jpg', 'datauri');
+				this.dom.container.addClass('mod-blur-shot');
+			}
 		}
 
-		_frame.modal.dom.container.addClass('on ' + settings.classname).data('customclass', settings.classname);
-		_p.initDOM(_frame.modal.dom.content);
+		this.dom.container.addClass('on ' + settings.classname).data('customclass', settings.classname);
+		_p.initDOM(this.dom.content);
 
-		_frame.modal.dom.bg.off('click.blank_to_close').on('click.blank_to_close', function () {
+		this.dom.bg.off('click.blank_to_close').on('click.blank_to_close', function () {
 			if (settings.blank_to_close) {
 				_frame.modal.dom.btn_close.trigger('click');
 			}
 		});
 
-		if (callback) callback(_frame.modal.dom.content);
+		if (callback) callback(this.dom.content);
 	},
 
 	hide: function hide() {
-		if (!_frame.modal.showing) return false;
+		if (!this.showing) return false;
 
-		clearTimeout(_frame.modal.hide_timeout);
-		_frame.modal.hide_timeout = null;
-		_frame.modal.dom.container.removeClass('on');
+		clearTimeout(this.hide_timeout);
+		this.hide_timeout = null;
+		this.dom.container.removeClass('on');
 	},
 
 	reset: function reset() {
-		_frame.modal.resetContent();
+		this.resetContent();
 
-		if (_frame.modal.dom.blured) {
-			if (!parseInt(_frame.modal.dom.container.css('opacity'))) {
-				_frame.modal.dom.blured.remove();
-				_frame.modal.dom.blured = null;
+		if (this.dom.blured) {
+			if (!parseInt(this.dom.container.css('opacity'))) {
+				this.dom.blured.remove();
+				this.dom.blured = null;
 			}
-			_frame.modal.dom.container.removeClass('bluredbg');
+			this.dom.container.removeClass('mod-blur-backdrop mod-blur-shot');
 		}
 	},
 
 	resetContent: function resetContent() {
-		_frame.modal.dom.content.empty();
+		this.dom.content.empty();
 
-		_frame.modal.dom.container.removeClass(_frame.modal.dom.container.data('customclass'));
-		_frame.modal.dom.container.data('customclass', '');
+		this.dom.container.removeClass(this.dom.container.data('customclass'));
+		this.dom.container.data('customclass', '');
 
-		_frame.modal.dom.titlebar.html('');
-		_frame.modal.dom.container.removeClass('hastitle');
+		this.dom.titlebar.html('');
+		this.dom.container.removeClass('hastitle');
 	}
 };
 
 _frame.modal.init = function () {
-	if (_frame.modal.is_init) return true;
+	if (this.is_init) return true;
 
-	_frame.modal.dom.container = $('<div class="modal" />').on({
+	this.dom.container = $('<div class="modal" />').on({
 		'transitionend.modal_hide webkitTransitionEnd.modal_hide mozTransitionEnd.modal_hide': function transitionendModal_hideWebkitTransitionEndModal_hideMozTransitionEndModal_hide(e) {
 			if (_frame.modal.showing && e.currentTarget == e.target && e.originalEvent.propertyName == 'opacity' && parseFloat($(this).css('opacity')) <= 0) {
 				_frame.modal.hide_timeout = setTimeout(function () {
@@ -2019,19 +2023,19 @@ _frame.modal.init = function () {
 			}
 		}
 	}).prependTo($body);
-	_frame.modal.dom.box = $('<div/>').appendTo(_frame.modal.dom.container);
-	_frame.modal.dom.titlebar = $('<header/>').appendTo(_frame.modal.dom.box);
-	_frame.modal.dom.content = $('<section/>').appendTo(_frame.modal.dom.box);
-	_frame.modal.dom.btn_close = $('<button class="close" />').html('&times;').on('click', function () {
+	this.dom.box = $('<div/>').appendTo(this.dom.container);
+	this.dom.titlebar = $('<header/>').appendTo(this.dom.box);
+	this.dom.content = $('<section/>').appendTo(this.dom.box);
+	this.dom.btn_close = $('<button class="close" />').html('&times;').on('click', function () {
 		_frame.modal.hide();
-	}).appendTo(_frame.modal.dom.box);
-	_frame.modal.dom.bg = $('<s/>').appendTo(_frame.modal.dom.container);
+	}).appendTo(this.dom.box);
+	this.dom.bg = $('<s/>').appendTo(this.dom.container);
 
 	_hotkey.bind('27', function () {
 		_frame.modal.hide();
 	});
 
-	_frame.modal.is_init = true;
+	this.is_init = true;
 	return true;
 };
 
