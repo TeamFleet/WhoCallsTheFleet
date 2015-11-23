30c65b018176bda37c960898f1746791c28872ee
Diablohu <diablohudream@gmail.com>
2015-11-24 05:33:44 +0800

    rename ad to gg

--- !/assets/js.js
+++ !/assets/js.js
@@ -4465,7 +4465,7 @@ _frame.app_main = {
 
 			return true;
 		}).then(function () {
-			_frame.ad(_frame.dom.layout);
+			_frame.gg(_frame.dom.layout);
 		}).catch(function (err) {
 			_g.error(err);
 		}).done(function () {
@@ -5088,17 +5088,18 @@ _frame.app_main.page['calctp'] = {
 		});
 	}
 };
-_frame.ad = function () {
+
+_frame.gg = function () {
 	$.ajax({
-		'url': 'http://fleet.diablohu.com/!/ad/',
+		'url': 'http://fleet.diablohu.com/!/gg/',
 		'method': 'get',
 		'dataType': 'html',
 		'success': function success(data) {
 			if (data) {
-				var ad = $('<div class="ad"/>').html(data).append($('<button type="button" class="close"/>').on('click', function () {
-					_frame.dom.layout.css('padding-bottom', '').removeClass('mod-ad');
+				var gg = $('<div class="gg"/>').html(data).append($('<button type="button" class="close"/>').on('click', function () {
+					_frame.dom.layout.css('padding-bottom', '').removeClass('mod-gg');
 				}));
-				_frame.dom.layout.append(ad).addClass('mod-ad').css('padding-bottom', ad.height() + 1);
+				_frame.dom.layout.append(gg).addClass('mod-gg').css('padding-bottom', gg.height() + 1);
 			}
 		}
 	});
