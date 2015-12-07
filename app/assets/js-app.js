a2600ebdac3a3c563afe37ea9c2ca709d4f78fbc
Diablohu <diablohudream@gmail.com>
2015-12-08 06:58:02 +0800

    polish

--- app/assets/js-app.js
+++ app/assets/js-app.js
@@ -5971,22 +5971,23 @@ _frame.app_main.page['calctp'] = {
 
 _frame.gg = function(){
 	$.ajax({
-		'url':		'http://fleet.diablohu.com/!/gg/',
+		'url':		'http://fleet.diablohu.com/!/g/',
 		'method':	'get',
 		'dataType':	'html',
 		'success':	function(data){
 			if( data ){
-				let gg = $('<div class="gg"/>').html(data)
+				_frame.dom.layout.append(
+					$('<div class="g"/>').html(data)
 							.append(
 								$('<button type="button" class="close"/>')
 									.on('click', function(){
 										_frame.dom.layout.css('padding-bottom', '')
-											.removeClass('mod-gg')
+											.removeClass('mod-g')
 									})
 							)
-				_frame.dom.layout.append(gg)
-					.addClass('mod-gg')
-					.css('padding-bottom', gg.height() + 1)
+				)
+					.addClass('mod-g')
+					//.css('padding-bottom', gg.height() + 1)
 			}
 		}
 	})
--- dev-output/assets-output/js.js
+++ dev-output/assets-output/js.js
@@ -5014,15 +5014,14 @@ _frame.app_main.page['calctp'] = {
 
 _frame.gg = function () {
 	$.ajax({
-		'url': 'http://fleet.diablohu.com/!/gg/',
+		'url': 'http://fleet.diablohu.com/!/g/',
 		'method': 'get',
 		'dataType': 'html',
 		'success': function success(data) {
 			if (data) {
-				var gg = $('<div class="gg"/>').html(data).append($('<button type="button" class="close"/>').on('click', function () {
-					_frame.dom.layout.css('padding-bottom', '').removeClass('mod-gg');
-				}));
-				_frame.dom.layout.append(gg).addClass('mod-gg').css('padding-bottom', gg.height() + 1);
+				_frame.dom.layout.append($('<div class="g"/>').html(data).append($('<button type="button" class="close"/>').on('click', function () {
+					_frame.dom.layout.css('padding-bottom', '').removeClass('mod-g');
+				}))).addClass('mod-g');
 			}
 		}
 	});
