b5b5f4000b900ca2fe436e43dda19afa3de05b34
Diablohu <diablohudream@gmail.com>
2016-07-01 01:45:07 +0800

    update

---
+++ !/tip/equipments/zh_cn/181.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",181,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/6.png)\"></s><strong>零式舰战32型</strong><small>舰载战斗机</small></h3><span>+5 对空</span><span>+2 回避</span><span>航程: 5</span>")
