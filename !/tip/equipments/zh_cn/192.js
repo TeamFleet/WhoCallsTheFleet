08104c2414999053ec915d1d89efa7381f3ccde9
Diablohu <diablohudream@gmail.com>
2016-10-07 18:22:37 +0800

    update

---
+++ !/tip/equipments/zh_cn/192.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",192,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/3.png)\"></s><strong>38.1cm Mk.I/N 连装炮改</strong><small>大口径主炮</small></h3><span>+19 火力</span><span>+3 对空</span><span>+2 命中</span><span>射程: 长</span>")
