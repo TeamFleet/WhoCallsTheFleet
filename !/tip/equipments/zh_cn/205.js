ac8f01a07e4b5ee41bac3cf8317f27f43594aa41
Diablohu <diablohudream@gmail.com>
2016-12-29 03:46:46 +0800

    update

---
+++ !/tip/equipments/zh_cn/205.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",205,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/6.png)\"></s><strong>F6F-3</strong><small>舰载战斗机</small></h3><span>+1 火力</span><span>+8 对空</span><span>+1 命中</span><span>+2 回避</span><span>+1 索敌</span><span>航程: 5</span>")
