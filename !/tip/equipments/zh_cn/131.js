81f2de74061489827edfc347209c618d7fd0e8bd
Diablohu <diablohudream@gmail.com>
2017-04-06 03:48:55 +0800

    update

--- !/tip/equipments/zh_cn/131.js
+++ !/tip/equipments/zh_cn/131.js
@@ -1,1 +1,1 @@
-KCTip.loaded("equipments",131,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/15.png)\"></s><strong>25mm三连装机枪 集中配备</strong><small>对空机枪 (集中配备)</small></h3><span>+9 对空</span><span>+1 回避</span>")
+KCTip.loaded("equipments",131,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/15.png)\"></s><strong>25mm三连装机枪 集中配备</strong><small>对空机枪 (强化)</small></h3><span>+9 对空</span><span>+1 回避</span>")
