b5b21574c1efafdd6fbad54f0269600caa62148e
Diablohu <diablohudream@gmail.com>
2017-05-30 01:09:11 +0800

    update

---
+++ !/tip/equipments/zh_cn/236.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",236,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/3.png)\"></s><strong>41cm三连装炮改</strong><small>大口径主炮</small></h3><span>+22 火力</span><span>+6 对空</span><span>+4 命中</span><span>+1 装甲</span><span>射程: 长</span>")
