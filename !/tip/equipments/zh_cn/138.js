9dd40fc210da0d9a6df3d118322bb04a60455bb3
Diablohu <diablohu@users.noreply.github.com>
2017-09-17 00:21:02 +0800

    update

--- !/tip/equipments/zh_cn/138.js
+++ !/tip/equipments/zh_cn/138.js
@@ -1,1 +1,1 @@
-KCTip.loaded("equipments",138,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/33.png)\"></s><strong>二式大艇</strong><small>大型水上飞艇</small></h3><span>+1 对潜</span><span>+1 命中</span><span>+12 索敌</span><span>航程: 20</span>")
+KCTip.loaded("equipments",138,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/33.png)\"></s><strong>二式大艇</strong><small>大型飞艇</small></h3><span>+1 对潜</span><span>+1 命中</span><span>+12 索敌</span><span>航程: 20</span>")
