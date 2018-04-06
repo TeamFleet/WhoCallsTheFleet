7f52af7b1f5071d7cd929c7905d7c78e367597aa
Diablohu <diablohu@users.noreply.github.com>
2018-04-07 02:49:55 +0800

    update

--- !/assets/js.js
+++ !/assets/js.js
@@ -3087,7 +3087,7 @@ if (location.hostname !== 'localhost' && location.hostname !== '127.0.0.1' && lo
     location.replace('http://fleet.moe' + location.pathname);
 }
 
-_g.db_version = '20180310';
+_g.db_version = '20180407';
 
 _g.bgimg_count=26;
 
