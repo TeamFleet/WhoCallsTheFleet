5ccc7271a6e5bc4d7c6997e1f9f7c41925097731
Diablohu <diablohu@users.noreply.github.com>
2018-04-29 00:05:36 +0800

    update

---
+++ !/tip/equipments/zh_cn/286.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",286,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/5.png)\"></s><strong>61cm四连装<small>(酸素)</small>鱼雷后期型</strong><small>鱼雷</small></h3><span>+11 雷装</span><span>+2 命中</span><span>+1 装甲</span><span>射程: 短</span>")
