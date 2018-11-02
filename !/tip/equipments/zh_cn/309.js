c324bb9a91f12561393a538541ed883bcc7eccaf
Diablohu <diablohu@users.noreply.github.com>
2018-11-03 00:30:15 +0800

    update

---
+++ !/tip/equipments/zh_cn/309.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",309,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/5.png)\"></s><strong>甲标的 丙型</strong><small>微型潜艇</small></h3><span>+14 雷装</span><span>+1 命中</span><span>+1 回避</span>")
