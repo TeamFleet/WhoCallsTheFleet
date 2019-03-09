2944d04b0eb513859be20453915096660030cc59
Diablohu <diablohu@users.noreply.github.com>
2019-03-09 21:38:41 +0800

    update

--- !/tip/equipments/zh_cn/22.js
+++ !/tip/equipments/zh_cn/22.js
@@ -1,1 +1,1 @@
-KCTip.loaded("equipments",22,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/6.png)\"></s><strong>烈风</strong><small>舰载战斗机</small></h3><span>+10 对空</span><span>航程: 5</span>")
+KCTip.loaded("equipments",22,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/6.png)\"></s><strong>试制烈风 后期型</strong><small>舰载战斗机</small></h3><span>+10 对空</span><span>航程: 5</span>")
