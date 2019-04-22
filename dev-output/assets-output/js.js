6ddf7d9595e9f523b3a4a5eb126e851e59604655
Diablohu <diablohu@users.noreply.github.com>
2019-04-23 00:42:17 +0800

    update kckit

--- dev-output/assets-output/js.js
+++ dev-output/assets-output/js.js
@@ -3092,7 +3092,7 @@ if (location.hostname !== 'localhost' && location.hostname !== '127.0.0.1' && lo
     location.replace('http://fleet.moe' + location.pathname);
 }
 
-_g.db_version = '20190403';
+_g.db_version = '20190423';
 
 _g.bgimg_count = 0;
 
--- source/js-app/main-web.js
+++ source/js-app/main-web.js
@@ -17,7 +17,7 @@
 
 
 // Global Variables
-    _g.db_version = '20190403';
+    _g.db_version = '20190423';
 
     _g.bgimg_count = 0;
 
