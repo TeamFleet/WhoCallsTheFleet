000aa0e27f825a276ff5132ef01afdb2d6e458de
Diablohu <diablohu@users.noreply.github.com>
2019-06-01 03:17:51 +0800

    update

---
+++ !/tip/equipments/zh_cn/342.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",342,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/8.png)\"></s><strong>流星改<small>(一航战)</small></strong><small>舰载鱼雷轰炸机</small></h3><span>+14 雷装</span><span>+2 对空</span><span>+6 对潜</span><span>+1 命中</span><span>+4 索敌</span><span>航程: 6</span>")
