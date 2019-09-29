ced3c92e421dfd247f0424250412bfdacdcbccce
Diablohu <diablohu@users.noreply.github.com>
2019-09-29 23:22:34 +0800

    Game update 2019/9/29

--- dev-output/assets-output/js.js
+++ dev-output/assets-output/js.js
@@ -3102,7 +3102,7 @@ if (location.hostname !== 'localhost' && location.hostname !== '127.0.0.1' && lo
     location.replace('http://fleet.moe' + location.pathname);
 }
 
-_g.db_version = '20190616';
+_g.db_version = '20190929';
 
 _g.bgimg_count = 0;
 
