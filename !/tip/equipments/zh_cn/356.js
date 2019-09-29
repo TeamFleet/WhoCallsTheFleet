b4234ae2d8071a1c7b8940043d0084f0a56d8e7d
Diablohu <diablohu@users.noreply.github.com>
2019-09-30 00:29:17 +0800

    update

---
+++ !/tip/equipments/zh_cn/356.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",356,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/2.png)\"></s><strong>8inch三连装炮 Mk.9</strong><small>中口径主炮</small></h3><span>+11 火力</span><span>+1 对空</span><span>-1 回避</span><span>射程: 中</span>")
