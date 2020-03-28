2f91ff0357ed7bba789cae34c9077c2a8d6490cb
Diablohu <diablohu@users.noreply.github.com>
2020-03-29 02:52:54 +0800

    update

---
+++ !/tip/equipments/zh_cn/367.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",367,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/10.png)\"></s><strong>剑鱼<small>(Swordfish)</small><small>(水上机型)</small></strong><small>水上轰炸机</small></h3><span>+2 火力</span><span>+5 雷装</span><span>+4 对潜</span><span>+5 爆装</span><span>+1 命中</span><span>+2 索敌</span><span>航程: 3</span>")
