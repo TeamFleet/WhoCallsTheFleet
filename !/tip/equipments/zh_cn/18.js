a35d79c913ee2063ad149e4c44448196850b7c59
Diablohu <Diablohu@users.noreply.github.com>
2020-05-06 22:34:42 +0800

    update

--- !/tip/equipments/zh_cn/18.js
+++ !/tip/equipments/zh_cn/18.js
@@ -1,1 +1,1 @@
-KCTip.loaded("equipments",18,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/8.png)\"></s><strong>流星</strong><small>舰载鱼雷轰炸机</small></h3><span>+10 雷装</span><span>+4 对潜</span><span>+1 索敌</span><span>航程: 6</span>")
+KCTip.loaded("equipments",18,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/8.png)\"></s><strong>流星</strong><small>舰载鱼雷轰炸机</small></h3><span>+10 雷装</span><span>+1 对空</span><span>+4 对潜</span><span>+1 索敌</span><span>航程: 6</span>")
