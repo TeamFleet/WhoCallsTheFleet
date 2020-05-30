ec57b6f5427696c4f4942d60ca4420d67a0c279d
Diablohu <Diablohu@users.noreply.github.com>
2020-05-31 02:13:22 +0800

    update

---
+++ !/tip/equipments/zh_cn/380.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",380,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/16.png)\"></s><strong>12.7cm连装高角炮改二</strong><small>小口径高角主炮</small></h3><span>+2 火力</span><span>+6 对空</span><span>+2 对潜</span><span>+1 命中</span><span>+2 回避</span><span>射程: 短</span>")
