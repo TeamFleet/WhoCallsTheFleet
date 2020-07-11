2c21866e3383e2e5b372c224ae89b7558703470e
Diablohu <Diablohu@users.noreply.github.com>
2020-07-12 03:08:38 +0800

    update

---
+++ !/tip/equipments/zh_cn/387.js
@@ -0,0 +1,1 @@
+KCTip.loaded("equipments",387,"zh_cn","<h3><s style=\"background-image: url(http://fleet.moe/!/assets/images/itemicon/2.png)\"></s><strong>6inch三连装速射炮 Mk.16 mod.2</strong><small>中口径主炮</small></h3><span>+8 火力</span><span>+3 对空</span><span>+4 命中</span><span>+1 装甲</span><span>+1 回避</span><span>射程: 中</span>")
